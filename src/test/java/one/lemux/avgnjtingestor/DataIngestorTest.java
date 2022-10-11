1f088bbe776587ec1c3699e6e612fa6c685abd70
LemuxOne <lemux.one@gmail.com>
2022-10-11 13:44:50 -0400

    add test case for input example data and query "CITY CARTAGENA"

--- src/test/java/one/lemux/avgnjtingestor/DataIngestorTest.java
+++ src/test/java/one/lemux/avgnjtingestor/DataIngestorTest.java
@@ -20,8 +20,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import one.lemux.avgnjtingestor.exceptions.EmptyInputFileException;
 import one.lemux.avgnjtingestor.exceptions.InvalidInputFileException;
 import static one.lemux.avgnjtingestor.IOStreamsManager.*;
@@ -37,6 +35,57 @@ import static org.junit.Assert.*;
  * @author lemux
  */
 public class DataIngestorTest {
+    
+    private String INPUT_EXAMPLE_DATA = """
+        F1 
+        D Erica Burns,BARCELONA,93654902Y 
+        D Lucy Mcgee,LONDON,51011156P 
+        D Mitchell Newton,SAN FRANCISCO,25384390A 
+        D Margarita Richards,LAS VEGAS,09877359D 
+        D Rhonda Hopkins,SAN FRANCISCO,54315871Z 
+        D Antonia Harper,LAS VEGAS,27466628M 
+        D Hilda Caldwell,LONDON,61682270L 
+        D Alexander Arnold,SAN FRANCISCO,21743514G 
+        D Cheryl Hawkins,LAS VEGAS,76878166E 
+        D Renee Anderson,BARCELONA,44340637H 
+        F2 
+        D Mitchell Newton ; LAS VEGAS ; 25384390-A 
+        D Margarita Richards ; NEW YORK ; 09877359-D 
+        D Rhonda Hopkins ; BARCELONA ; 54315871-Z 
+        D Taylor Matthews ; LISBOA ; 58202263-G 
+        D Shelley Payne ; MADRID ; 54808168-L 
+        D Johnathan Cooper ; PARIS ; 10863096-N 
+        F1 
+        D Lowell Miles,BARCELONA,04217040J 
+        D Russell Pope,BARCELONA,69429384C 
+        D Shelley Payne,BARCELONA,54808168L 
+        D Johnathan Cooper,BARCELONA,10863096N 
+        D Myra Maldonado,LAS VEGAS,32445934H 
+        D Irene Owen,LONDON,15015516N 
+        D Susan Holland,SAN FRANCISCO,04810023X 
+        D Rodolfo West,LAS VEGAS,74176315G 
+        D Peter Daniel,BARCELONA,58204706D 
+        F2 
+        D Russell Pope ; CARTAGENA ; 69429384-C 
+        D Shelley Payne ; OVIEDO ; 54808168-L 
+        D Johnathan Cooper ; SANTANDER ; 10863096-N 
+        D Myra Maldonado ; MARSELLA ; 32445934-H 
+        D Glenn Bryan ; LISBON ; 90844421-X 
+        D Neal Love ; SEVILLA ; 52498689-Q 
+        D Taylor Matthews ; LONDRES ; 58202263-G 
+        F1 
+        D Marta Mendez,LAS VEGAS,17200667W 
+        D Glenn Bryan,LAS VEGAS,90844421X 
+        D Neal Love,LAS VEGAS,52498689Q 
+        D Taylor Matthews,LAS VEGAS,58202263G 
+        D Tommie Lindsey,LAS VEGAS,12020245P 
+        D Ruben Daniels,BARCELONA,84604786E 
+        D Emilio Warner,BARCELONA,23803975X 
+        D Mark Quinn,LAS VEGAS,82098573G 
+        D Dwight Roy,LONDON,87179151C
+        D Jake Salazar,SAN FRANCISCO,38399984N 
+        D Edna Soto,LAS VEGAS,66991455E 
+        """;
 
     public DataIngestorTest() {
     }
@@ -89,5 +138,40 @@ public class DataIngestorTest {
         }
 
     }
-
+    
+    /**
+     * Test of ingest method, of class DataIngestor.
+     */
+    @Test
+    public void testIngest_withExampleDataAndCityQuery() {
+        System.out.println("ingest_withExampleDataAndCityQuery");
+        try {
+            System.out.println("Preparing temp file with content");
+            var tmpPath = Files.createTempFile(null, "input.txt");
+            Files.writeString(tmpPath, INPUT_EXAMPLE_DATA, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+            
+            System.out.println("Ingesting given temp file as input: " + tmpPath.toString());
+            var query = new DataQuery("CITY", "CARTAGENA");
+            DataIngestor instance = new DataIngestor(tmpPath.toString());
+            
+            var expected = """
+                Mitchell Newton,25384390A 
+                Rhonda Hopkins,54315871Z 
+                Alexander Arnold,21743514G 
+                Susan Holland,04810023X 
+                Jake Salazar,38399984N
+                """;
+            
+            captureStreams();
+            instance.ingest(query);
+            var result = outStream.toString();
+            restoreStreams();
+            Files.deleteIfExists(tmpPath);
+            assertEquals(expected, result);
+        } catch (IOException ex) {
+            fail(ex.getMessage());
+        } catch (InvalidInputFileException | EmptyInputFileException ex) {
+            fail("The temp input file was empty or not accessible");
+        }
+    }
 }
