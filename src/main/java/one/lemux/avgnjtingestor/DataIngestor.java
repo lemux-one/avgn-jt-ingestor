e1ad8dd110405ba37fee540c68674c1ea0d2ef9b
LemuxOne <lemux.one@gmail.com>
2022-10-11 13:35:35 -0400

    remove unused import

--- src/main/java/one/lemux/avgnjtingestor/DataIngestor.java
+++ src/main/java/one/lemux/avgnjtingestor/DataIngestor.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import one.lemux.avgnjtingestor.exceptions.EmptyInputFileException;
 import one.lemux.avgnjtingestor.exceptions.InvalidInputFileException;
 
